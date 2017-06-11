ffafa67ff8302d6a4d1ee4645fa18745b691619b
Eralp Karaduman <eralpkaraduman@gmail.com>
2017-06-11 05:17:52 +0300

    added spotify auth with safarivc

--- app/screens/LogInScreen.js
+++ app/screens/LogInScreen.js
@@ -1,11 +1,12 @@
 'use_strict';
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, Linking } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import { Redirect } from 'react-router';
 import { WebView } from 'react-native';
 import styles from '../styles';
+import SafariView from 'react-native-safari-view';
 
 class LogInScreen extends React.Component {
   constructor(props, context) {
@@ -13,17 +14,32 @@ class LogInScreen extends React.Component {
   }
 
   handleLogInTapped = () => {
-    this.props.dispatch(actions.requestSpotifyLogIn());
-    // SafariView.isAvailable()
-    //   .then(SafariView.show({
-    //     url: 'https://accounts.spotify.com/authorize'
-    //   }))
-    //   .catch(error => {
-    //     // Fallback WebView code for iOS 8 and earlier
-    //   });
+
+    // this.props.dispatch(actions.requestSpotifyLogIn());
+    SafariView.isAvailable()
+      .then(SafariView.show({
+        url: 'https://accounts.spotify.com/authorize?client_id=e55f0a2b96cb4d7689be6812106713cf&response_type=token&redirect_uri=terminal-bar://spotify-authorize-callback&state=STATE&scopes=streaming&show_dialog=true'
+      }))
+      .catch(error => {
+        // Fallback WebView code for iOS 8 and earlier
+      });
   }
 
-  // https://accounts.spotify.com/authorize
+  componentDidMount() {
+    Linking.addEventListener('url', this._handleOpenURL);
+  }
+
+  componentWillUnmount() {
+    Linking.removeEventListener('url', this._handleOpenURL);
+  }
+
+  _handleOpenURL(event) {
+    console.log(event.url);
+    if (true) {
+      SafariView.dismiss();
+    }
+  }
+
   render() {
     if (this.props.isLoggedIn) {
       return <Redirect push to="/"/>;
